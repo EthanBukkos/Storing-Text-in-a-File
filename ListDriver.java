9b6e81282fc00e4bdd70c7a309a5ae85f0c551ff
Ethan Bukkos <126908264+EthanBukkos@users.noreply.github.com>
2023-06-06 21:50:11 -0700

    Update ListDriver.java

--- ListDriver.java
+++ ListDriver.java
@@ -6,9 +6,6 @@ public class ListDriver
 	public static void main (String [] args) throws IOException
 	{
 		ListWriter list = new ListWriter();
-		SortAlgorithms sort = new SortAlgorithms();
-		
 		list.WriteList();
-		
 	}
 }
