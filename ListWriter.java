7f1239f36d31b1dd644a2a18de8f7b6be94ca971
Ethan Bukkos <126908264+EthanBukkos@users.noreply.github.com>
2023-06-06 17:08:44 -0700

    Update ListWriter.java

--- ListWriter.java
+++ ListWriter.java
@@ -10,20 +10,20 @@ public class ListWriter
 		Scanner scan1 = new Scanner(System.in);
 		Scanner scan2 = new Scanner(System.in);
 		Scanner scan3 = new Scanner (System.in);
-		int cap;
+		int capacity;
 		String object = "";
 		
 		System.out.print("How many objects would you like to store? : ");
 		cap = scan1.nextInt();
 		
-		while(cap < 1)
+		while(capacity < 1)
 		{
 			System.out.print("Enter a value greater than 0: ");
-			cap = scan1.nextInt();
+			capacity = scan1.nextInt();
 		}
 		pw.println(cap);
 		
-		for (int i = 1; i <= cap; i++) {
+		for (int i = 1; i <= capacity; i++) {
 			System.out.println("Enter an object: ");
 			object = scan3.nextLine();
 			pw.println(object);
