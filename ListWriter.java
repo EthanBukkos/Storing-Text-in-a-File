cdbb57940aa58802d8f583da5619fc5dcca3f130
Ethan Bukkos <126908264+EthanBukkos@users.noreply.github.com>
2023-06-08 14:48:10 -0700

    Updated ListWriter.java to handle InputMismatchException

--- ListWriter.java
+++ ListWriter.java
@@ -1,33 +1,52 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
 
 public class ListWriter
 {
+	private int capacity; 
+	
 	public void WriteList () throws IOException
 	{
 		File outputFile = new File("list.txt");
 		PrintWriter pw = new PrintWriter(outputFile);
-		Scanner scan = new Scanner(System.in);
-		int capacity;
+		Scanner scanCapacity = new Scanner(System.in);
+		Scanner scanObject = new Scanner(System.in);
 		String object = "";
+		boolean done = false;
 		
 		System.out.print("How many objects would you like to store? : ");
-		capacity = scan.nextInt();
 		
-		while(capacity < 1)
+		while (!done)
 		{
-			System.out.print("Enter a value greater than 0: ");
-			capacity = scan.nextInt();
-		}
-		pw.println(capacity);
+			
+			try
+			{
+				capacity = scanCapacity.nextInt();
+			
+				while(capacity < 1)
+				{
+					done = true;
+					System.out.print("Enter a value greater than 0: ");
+					capacity = scanCapacity.nextInt();
+				}
+			
+				pw.println("# of Items: " + capacity);
+			}
 		
-		for (int i = 1; i <= capacity; i++) {
-			System.out.println("Enter an object: ");
-			object = scan3.nextLine();
+			catch (InputMismatchException  ime )
+			{	
+				scanCapacity.nextLine();
+				System.out.println("You entered bad data. ");
+				System.out.print("Try again: ");
+			}
+		}
+		
+		for (int i = 0; i < capacity; i++)
+		{
+			System.out.print("Enter an object: ");
+			object = scanObject.nextLine();
 			pw.println(object);
 		}
-
-		pw.close();
-		
-}
+			pw.close();	
+	}
 }
